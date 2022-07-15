6d46fec51a4cb80f39978ae387f0dea71cd8d477
Domenicobrz <domenicobrz@gmail.com>
2022-07-15 03:11:32 +0200

    project init - testing envmap

---
+++ r3f-in-practice/src/SceneContainer.jsx
@@ -0,0 +1,14 @@
+import { OrbitControls, PerspectiveCamera, Environment } from "@react-three/drei";
+import { Suspense } from "react";
+
+export function SceneContainer() {
+  return (
+    <Suspense fallback={null}>
+      <Environment background={"only"} files={process.env.PUBLIC_URL + "textures/bg.hdr"} />
+      <Environment background={false} files={process.env.PUBLIC_URL + "textures/envmap.hdr"} />
+
+      <PerspectiveCamera makeDefault fov={50} position={[-1.75, 10.85, 20.35]} />
+      <OrbitControls target={[1, 5, 0]} maxPolarAngle={Math.PI * 0.5}/>
+    </Suspense>
+  );
+}
