9f525119e2fcf4a8eb3466a8c12e06ef2bfdbcb0
Domenicobrz <domenicobrz@gmail.com>
2022-10-29 18:50:23 +0200

    updated build

--- car-physics/src/index.js
+++ car-physics/src/index.js
@@ -5,13 +5,22 @@ import { Scene } from "./Scene";
 import { Physics } from "@react-three/cannon";
 
 createRoot(document.getElementById("root")).render(
-  <Canvas>
-    <Physics
-      broadphase="SAP"
-      allowSleep
-      gravity={[0, -2.6, 0]}
-    >
-      <Scene />
-    </Physics>
-  </Canvas>
+  <>
+    <Canvas>
+      <Physics
+        broadphase="SAP"
+        allowSleep
+        gravity={[0, -2.6, 0]}
+      >
+        <Scene />
+      </Physics>
+    </Canvas>
+
+    <div class="controls">
+      <p>press w a s d to move</p>
+      <p>press k to swap camera</p>
+      <p>press r to reset</p>
+      <p>press arrows for flips</p>
+    </div>
+  </>
 );
